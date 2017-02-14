fae4ec880d8bb092964fa9d8481039840996b97b
gjieoo <15202979397@163.com>
2017-02-14 14:21:08 +0800

    use ui.router

---
+++ ele/test/app.js
@@ -0,0 +1,21 @@
+var myApp = angular.module("myApp", ["ui.router"]);
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("", "/pageTab");
+    $stateProvider
+        .state("pageTab", {
+            url: "/pageTab",
+            templateUrl: "pageTab.html"
+        })
+        .state("pageTab.page1", {
+            url: "/page1",
+            templateUrl: "page1.html"
+        })
+        .state("pageTab.page2", {
+            url: "/page2",
+            templateUrl: "page2.html"
+        })
+        .state("pageTab.page3", {
+            url: "/page3",
+            templateUrl: "page3.html"
+        });
+});
